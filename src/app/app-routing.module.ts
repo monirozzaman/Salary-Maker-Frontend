05263104d5bc85a2a50141bb7fd68ed8a9e9a7cb
monirozzamanroni <monirozzaman35-223@diu.edu.bd>
2020-11-10 10:00:49 +0600

    'add-components'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import {HomeComponent} from './components/home/home.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {EmployeesComponent} from './components/employees/employees.component';
 import {SalaryMakerComponent} from './components/salary-maker/salary-maker.component';
+import {AddNewEmployeeComponent} from './components/add-new-employee/add-new-employee.component';
+import {EmployeesListComponent} from './components/employees-list/employees-list.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent, children: [
       {path: 'dashboard', component: DashboardComponent},
-      {path: 'employees', component: EmployeesComponent},
+      {path: 'employees', component: EmployeesComponent, children: [
+          {path: 'add-new-employee' , component: AddNewEmployeeComponent},
+          {path: 'employees-list' , component: EmployeesListComponent}
+        ]},
       {path: 'employee-salary', component: SalaryMakerComponent}
     ]}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { HomeComponent } from './components/home/home.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { SalaryMakerComponent } from './components/salary-maker/salary-maker.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {Route, RouterModule} from '@angular/router';
+import { EmployeesListComponent } from './components/employees-list/employees-list.component';
+import { AddNewEmployeeComponent } from './components/add-new-employee/add-new-employee.component';
 
 @NgModule({
   declarations: [
@@ -15,11 +18,15 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     HomeComponent,
     EmployeesComponent,
     SalaryMakerComponent,
-    DashboardComponent
+    DashboardComponent,
+    EmployeesListComponent,
+    AddNewEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
